9901a23c31105ac6828cccf9c6a1362baec2866f
RhonyGS <44628214+RhonyGS@users.noreply.github.com>
2018-11-04 23:37:31 -0400

    Segunda actividad

--- app/src/main/java/android/example/com/BuyIt/MainActivity.java
+++ app/src/main/java/android/example/com/BuyIt/MainActivity.java
@@ -1,6 +1,7 @@
 package android.example.com.BuyIt;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -52,6 +53,85 @@ public class MainActivity extends AppCompatActivity {
         m8 = (TextView) findViewById(R.id.text_message_reply8);
         m9 = (TextView) findViewById(R.id.text_message_reply9);
         m10 = (TextView) findViewById(R.id.text_message_reply10);
+        m11 = (TextView) findViewById(R.id.text_message_reply11);
+        m12 = (TextView) findViewById(R.id.text_message_reply12);
+        m13 = (TextView) findViewById(R.id.text_message_reply13);
+        m14 = (TextView) findViewById(R.id.text_message_reply14);
+        m15 = (TextView) findViewById(R.id.text_message_reply15);
+        m16 = (TextView) findViewById(R.id.text_message_reply16);
+        m17 = (TextView) findViewById(R.id.text_message_reply17);
+        m18 = (TextView) findViewById(R.id.text_message_reply18);
+        m19 = (TextView) findViewById(R.id.text_message_reply19);
+        m20 = (TextView) findViewById(R.id.text_message_reply20);
+
+
+        if (savedInstanceState != null) {
+
+            boolean isVisible = savedInstanceState.getBoolean("reply_visible");
+
+            if (isVisible) {
+
+                m1.setText(savedInstanceState.getString("reply_m1"));
+                m1.setVisibility(View.VISIBLE);
+
+                m2.setText(savedInstanceState.getString("reply_m2"));
+                m2.setVisibility(View.VISIBLE);
+
+                m3.setText(savedInstanceState.getString("reply_m3"));
+                m3.setVisibility(View.VISIBLE);
+
+                m4.setText(savedInstanceState.getString("reply_m4"));
+                m4.setVisibility(View.VISIBLE);
+
+                m5.setText(savedInstanceState.getString("reply_m5"));
+                m5.setVisibility(View.VISIBLE);
+
+                m6.setText(savedInstanceState.getString("reply_m6"));
+                m6.setVisibility(View.VISIBLE);
+
+                m7.setText(savedInstanceState.getString("reply_m7"));
+                m7.setVisibility(View.VISIBLE);
+
+                m8.setText(savedInstanceState.getString("reply_m8"));
+                m8.setVisibility(View.VISIBLE);
+
+                m9.setText(savedInstanceState.getString("reply_m9"));
+                m9.setVisibility(View.VISIBLE);
+
+                m10.setText(savedInstanceState.getString("reply_m10"));
+                m10.setVisibility(View.VISIBLE);
+
+                m11.setText(savedInstanceState.getString("reply_m11"));
+                m11.setVisibility(View.VISIBLE);
+
+                m12.setText(savedInstanceState.getString("reply_m12"));
+                m12.setVisibility(View.VISIBLE);
+
+                m13.setText(savedInstanceState.getString("reply_m13"));
+                m13.setVisibility(View.VISIBLE);
+
+                m14.setText(savedInstanceState.getString("reply_m14"));
+                m14.setVisibility(View.VISIBLE);
+
+                m15.setText(savedInstanceState.getString("reply_m15"));
+                m15.setVisibility(View.VISIBLE);
+
+                m16.setText(savedInstanceState.getString("reply_m16"));
+                m16.setVisibility(View.VISIBLE);
+
+                m17.setText(savedInstanceState.getString("reply_m17"));
+                m17.setVisibility(View.VISIBLE);
+
+                m18.setText(savedInstanceState.getString("reply_m18"));
+                m18.setVisibility(View.VISIBLE);
+
+                m19.setText(savedInstanceState.getString("reply_m19"));
+                m19.setVisibility(View.VISIBLE);
+
+                m20.setText(savedInstanceState.getString("reply_m20"));
+                m20.setVisibility(View.VISIBLE);
+            }
+        }
     }
 
     public void launchSecondActivity(View view) {
@@ -63,6 +143,144 @@ public class MainActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode,Intent data){
         super.onActivityResult(requestCode, resultCode, data);
 
-    }
+        if (requestCode == TEXT_REQUEST) {
 
+
+            if (resultCode == RESULT_OK) {
+
+                if(count==1){
+
+                    String addart1 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m1.setText(addart1);
+                }
+
+                if(count==2){
+
+                    String addart2 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m2.setText(addart2);
+                }
+
+                if(count==3){
+
+                    String addart3 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m3.setText(addart3);
+                }
+
+                if(count==4){
+
+                    String addart4 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m4.setText(addart4);
+                }
+
+                if(count==5){
+
+                    String addart5 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m5.setText(addart5);
+                }
+
+                if(count==6){
+
+                    String addart6 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m6.setText(addart6);
+                }
+
+                if(count==7){
+
+                    String addart7 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m7.setText(addart7);
+                }
+
+                if(count==8){
+
+                    String addart8 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m8.setText(addart8);
+                }
+
+                if(count==9){
+
+                    String addart9 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m9.setText(addart9);
+                }
+
+                if(count==10){
+
+                    String addart10 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m10.setText(addart10);
+                    Toast.makeText(MainActivity.this,   "Carrito lleno, compre ahora", Toast.LENGTH_SHORT).show();
+                }
+
+                if(count==11){
+
+                    String addart11 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m11.setText(addart11);
+                    Toast.makeText(MainActivity.this,   "Carrito lleno, compre ahora", Toast.LENGTH_SHORT).show();
+                }
+
+                if(count==12){
+
+                    String addart12 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m12.setText(addart12);
+                    Toast.makeText(MainActivity.this,   "Carrito lleno, compre ahora", Toast.LENGTH_SHORT).show();
+                }
+
+                if(count==13){
+
+                    String addart13 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m13.setText(addart13);
+                    Toast.makeText(MainActivity.this,   "Carrito lleno, compre ahora", Toast.LENGTH_SHORT).show();
+                }
+
+                if(count==14){
+
+                    String addart14 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m14.setText(addart14);
+                    Toast.makeText(MainActivity.this,   "Carrito lleno, compre ahora", Toast.LENGTH_SHORT).show();
+                }
+
+                if(count==15){
+
+                    String addart15 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m15.setText(addart15);
+                    Toast.makeText(MainActivity.this,   "Carrito lleno, compre ahora", Toast.LENGTH_SHORT).show();
+                }
+
+                if(count==16){
+
+                    String addart16 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m16.setText(addart16);
+                    Toast.makeText(MainActivity.this,   "Carrito lleno, compre ahora", Toast.LENGTH_SHORT).show();
+                }
+
+                if(count==17){
+
+                    String addart17 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m17.setText(addart17);
+                    Toast.makeText(MainActivity.this,   "Carrito lleno, compre ahora", Toast.LENGTH_SHORT).show();
+                }
+
+                if(count==18){
+
+                    String addart18 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m18.setText(addart18);
+                    Toast.makeText(MainActivity.this,   "Carrito lleno, compre ahora", Toast.LENGTH_SHORT).show();
+                }
+
+                if(count==19){
+
+                    String addart19 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m19.setText(addart19);
+                    Toast.makeText(MainActivity.this,   "Carrito lleno, compre ahora", Toast.LENGTH_SHORT).show();
+                }
+
+                if(count==20){
+
+                    String addart20 = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
+                    m10.setText(addart20);
+                    Toast.makeText(MainActivity.this,   "Carrito lleno, compre ahora", Toast.LENGTH_SHORT).show();
+                }
+
+                count = count + 1;
+            }
+        }
+    }
 }
---
+++ app/src/main/java/android/example/com/BuyIt/SecondActivity.java
@@ -0,0 +1,244 @@
+package android.example.com.BuyIt;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "android.example.com.BuyIt.extra.MESSAGE";
+    private TextView appwatc;
+    private TextView aud;
+    private TextView lapt;
+    private TextView corn;
+    private TextView drone;
+    private TextView cam;
+    private TextView discod;
+    private TextView ps4;
+    private TextView xbox;
+    private TextView tablet;
+    private TextView pendr;
+    private TextView tv;
+    private TextView rout;
+    private TextView swit;
+    private TextView repet;
+    private TextView imp;
+    private TextView mouse;
+    private TextView pbank;
+    private TextView cel;
+    private TextView pc;
+
+    public SecondActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        appwatc = (TextView) findViewById(R.id.apple_watch);
+        aud = (TextView) findViewById(R.id.audifonos);
+        lapt = (TextView) findViewById(R.id.laptop);
+        corn = (TextView) findViewById(R.id.corneta);
+        drone = (TextView) findViewById(R.id.drone);
+        cam = (TextView) findViewById(R.id.camara);
+        discod = (TextView) findViewById(R.id.discoduro);
+        ps4 = (TextView) findViewById(R.id.ps4);
+        xbox = (TextView) findViewById(R.id.xbox);
+        tablet = (TextView) findViewById(R.id.tablet);
+        pendr = (TextView) findViewById(R.id.pendrive);
+        tv = (TextView) findViewById(R.id.tv);
+        rout = (TextView) findViewById(R.id.router);
+        swit = (TextView) findViewById(R.id.conmutador);
+        repet = (TextView) findViewById(R.id.repetidor);
+        imp = (TextView) findViewById(R.id.impresora);
+        mouse = (TextView) findViewById(R.id.mouse);
+        pbank = (TextView) findViewById(R.id.powerbank);
+        cel = (TextView) findViewById(R.id.celular);
+        pc = (TextView) findViewById(R.id.pc);
+    }
+
+    public void compraAw(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart1 = appwatc.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart1);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraAud(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart2 = aud.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart2);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraLap(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart3 = lapt.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart3);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraCor(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart4 = corn.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart4);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraDrone(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart5 = drone.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart5);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraCam(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart6 = cam.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart6);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraDisc(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart7 = discod.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart7);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraPs4(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart8 = ps4.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart8);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraXbox(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart9 = xbox.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart9);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraTab(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart10 = tablet.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart10);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraPend(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart11 = pendr.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart11);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraTv(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart12 = tv.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart12);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraRout(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart13 = rout.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart13);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraConmu(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart14 = swit.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart14);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraRep(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart15 = repet.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart15);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraImp(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart16 = imp.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart16);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraMou(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart17 = mouse.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart17);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraPow(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart18 = pbank.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart18);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraCel(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart19 = cel.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart19);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+
+    public void compraPc(View view) {
+        Toast.makeText(SecondActivity.this,   "Artículo agregado", Toast.LENGTH_SHORT).show();
+        String addart20 = pc.getText().toString();
+        Intent addintent = new Intent();
+        addintent.putExtra(EXTRA_MESSAGE,addart20);
+        setResult(RESULT_OK,addintent);
+        finish();
+    }
+}
